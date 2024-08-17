7af182bead6a67511933c2d33d88e2c1e7a8e317
Eugene-36 <livenskiyjenya@gmail.com>
2024-08-17 23:19:40 +0300

    [cors issue]

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('./db/userModel');
+const auth = require('./auth');
 const express = require('express');
 const app = express();
 const { port } = require('./config');
 
 const connection = require('./db/connect');
 connection();
-
+// Handle corse issue
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
 //Need to parse body
 app.use(express.json());
 // config file where all env variables
@@ -17,7 +31,7 @@ app.get('/', (req, res) => {
 app.get('/next', (req, res) => {
   res.send('Work');
 });
-
+// Register Endpoint
 app.post('/register', (req, res) => {
   console.log('BODY', req.body);
 
@@ -50,7 +64,51 @@ app.post('/register', (req, res) => {
       });
     });
 });
+// Login Endpoint
+app.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((passwordCheck) => {
+          if (!passwordCheck) {
+            return res.status(404).send({
+              message: 'Passwords does not match',
+              error,
+            });
+          }
 
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            'RANDOM-TOKEN',
+            { expiresIn: '24h' }
+          );
+          res.status(200).send({
+            message: 'Login Successful',
+            email: user.email,
+            token,
+          });
+        })
+        .catch((e) => {
+          res.status(404).send({ message: 'Password does not match' });
+        });
+    })
+    .catch((e) => {
+      res.status(404).send({ message: 'Email not found' });
+    });
+});
+
+// free endpoint
+app.get('/free-endpoint', (req, res) => {
+  res.json({ message: 'You are free to access me anytime' });
+});
+// authentication endpoint
+app.get('/auth-endpoint', auth, (req, res) => {
+  res.json({ message: 'You are authorized to access me' });
+});
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
