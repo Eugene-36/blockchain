6c450ec9aa81cad1a7298ce403b919c8300336f4
Eugene-36 <livenskiyjenya@gmail.com>
2024-08-17 13:55:27 +0300

    [some updates]

--- app.js
+++ app.js
@@ -1,23 +1,54 @@
+const bcrypt = require('bcrypt');
+const User = require('./db/userModel');
 const express = require('express');
 const app = express();
 const { port } = require('./config');
 
 const connection = require('./db/connect');
 connection();
+
+//Need to parse body
+app.use(express.json());
 // config file where all env variables
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.get('/next', (req, res) => {
-  res.send('Normal NORMAL NORMAL NORMAL');
+  res.send('Work');
 });
-// app.post('/submit-form', (req, res) => {
-//   res.send('Form submitted');
-// });
-app.use((req, res, next) => {
-  console.log(`${req.method} request for ${req.url}`);
-  next();
+
+app.post('/register', (req, res) => {
+  console.log('BODY', req.body);
+
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: req.body.email,
+        password: hashedPassword,
+      });
+      user
+        .save()
+        .then((result) => {
+          res.status(201).send({
+            message: 'User Created Successfully',
+            result,
+          });
+        })
+        .catch((error) => {
+          res.status(500).send({
+            message: 'Error creating user',
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      res.status(500).send({
+        message: 'Password was not hashed successfully',
+        e,
+      });
+    });
 });
 
 app.listen(port, () => {
